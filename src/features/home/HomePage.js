5b7fe6149ff54a5e0b54f726a2d4d5124eced00b
yanivsultan <yanivsultan5@gmail.com>
2020-03-23 04:12:20 +0200

    routing added includes internal navigation

---
+++ src/features/home/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Segment, Header, Container, Image, Button, Icon } from 'semantic-ui-react';
+
+const HomePage = (props) => {
+    return (<Segment inverted textAlign='center' vertical className='masthead'>
+            <Container text>
+              <Header as='h1' inverted>
+                <Image
+                  size='massive'
+                  src='/assets/logo.png'
+                  alt='logo'
+                  style={{ marginBottom: 12 }}
+                />
+                Re-vents
+              </Header>
+              <Button onClick={() => props.history.push('/events')} size='huge' inverted>
+                Get started
+                <Icon name='right arrow' inverted />
+              </Button>
+            </Container>
+          </Segment>)
+}
+
+export default HomePage;
--- src/features/nav/NavBar/NavBar.js
+++ src/features/nav/NavBar/NavBar.js
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "../Menus/SignedOutMenu";
+import SignedInMenu from "../Menus/SignedInMenu";
 
 class NavBar extends Component {
+  state = {
+    authenticated: false
+  };
+
+  handleSignIn = () => this.setState({ authenticated: true });
+  handleSignOut = () => {
+    this.setState({ authenticated: false });
+    this.props.history.push('/')
+  }
+
   render() {
+    const authenticated = this.state.authenticated;
     return (
       <Menu inverted fixed='top'>
         <Container>
-          <Menu.Item header className='menu_logo'>
+          <Menu.Item as={NavLink} exact to='/' header className='menu_logo'>
             <img src='assets/logo.png' alt='logo' width='100px' />
           </Menu.Item>
-          <Menu.Item name='Events' />
-          <Menu.Item>
+          <Menu.Item as={NavLink} to='/events' name='Events' />
+          <Menu.Item as={NavLink} to='/people' name='People' />
+          <Menu.Item as={Link} to='/createEvent'>
             <Button floated='right' positive inverted content='Create Event' />
           </Menu.Item>
-          <Menu.Item position='right'>
-            <Button basic inverted content='Login' />
-            <Button
-              basic
-              inverted
-              content='Sign Out'
-              style={{ marginLeft: "0.5em" }}
-            />
-          </Menu.Item>
+          {authenticated ? <SignedInMenu signOut={this.handleSignOut} /> : <SignedOutMenu signIn={this.handleSignIn} />}
         </Container>
       </Menu>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
